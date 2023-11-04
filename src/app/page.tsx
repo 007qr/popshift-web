1cd7db42fc527efbe55af64a177cfcea54d164c8
Ayush Patil <aayushpatil558321@gmail.com>
2023-11-05 01:11:57 +0530

    added layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,156 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useTransform, useScroll, motion, MotionValue } from "framer-motion";
+import { useRef } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const containerRef = useRef<HTMLDivElement>(null);
+    const containerRef2 = useRef<HTMLDivElement>(null);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ["start end", "end start"],
+    });
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const container2Scroll = useScroll({
+      target: containerRef2,
+      offset: ['center', 'start end']
+    })
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const y = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+    const y2 = useTransform(scrollYProgress, [0, 1], [0, 1000 * -1.2]);
+    const y3 = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+    const y4 = useTransform(container2Scroll.scrollYProgress, [0, 1], [2000, -100]);
+    const y5 = useTransform(container2Scroll.scrollYProgress, [0, 1], [300, 0]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <div className="h-[70svh]">
+                <div className="sticky top-0 text-center">
+                    <h1 className="text-7xl tracking-tighter font-bold">
+                        TV Shows
+                    </h1>
+                </div>
+            </div>
+            <div className="flex justify-center mt-2">
+                <p className="w-60 text-center">
+                    Alongside showrunners and writers of some of{" "}
+                    {"television's"} most popular shows
+                </p>
+            </div>
+            <div
+                className="h-[155vh] mt-[15vh] overflow-hidden"
+                ref={containerRef}
+            >
+                <div className="w-full h-full flex flex-col gap-[1vh] relative">
+                    <Row
+                        left="-55%"
+                        y={y}
+                        images={[
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                        ]}
+                    />
+                    <Row
+                        left="15%"
+                        y={y2}
+                        images={[
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                        ]}
+                    />
+                    <Row
+                        left="-95%"
+                        y={y3}
+                        images={[
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                            "/images/1.png",
+                        ]}
+                    />
+                </div>
+            </div>
+            <div className="h-[80svh]">
+                <h1
+                    className="text-7xl text-center sticky top-10"
+                    style={{ fontFamily: "Times New Roman" }}
+                >
+                    The Layout
+                </h1>
+            </div>
+            <div className="h-[70vh] flex-col flex">
+              <div className="flex flex-col items-center">
+                <p className="mt-7 w-72 text-center"> 
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Repudiandae dolorum inventore praesentium, vel eum doloribus
+                    nulla magnam beatae modi pariatur.
+                </p>
+              </div>
+              <motion.div className="flex mx-10 justify-center overflow-hidden" style={{marginTop: y5}}>
+                  <motion.div style={{marginRight: y4}} className="flex mr-[90%] flex-col items-center overflow-hidden" id='decrease-margin'>
+                      <h1 className="text-4xl">50</h1>
+                      <p>of {"world's"} most <br /> fascinating minds and</p>
+                  </motion.div>
+                  <div className="flex flex-col items-center">
+                      <h1 className="text-4xl">50</h1>
+                      <p>of {"world's"} most <br /> fascinating minds and</p>
+                  </div>
+              </motion.div>
+            </div>
+            <div className="h-screen mt-7"></div>
+        </>
+    );
 }
+
+const Row = ({
+    images,
+    y,
+    left,
+}: {
+    images: string[];
+    y: MotionValue<number>;
+    left: string;
+}) => {
+    return (
+        <motion.div
+            style={{ x: y, left: left }}
+            className="w-min h-min flex-row flex gap-[1vh] relative"
+        >
+            {images.map((v, i) => {
+                return (
+                    <div key={i} className="relative w-[250px] h-[340px]">
+                        <Image
+                            src={v}
+                            fill
+                            alt={`${i}`}
+                            className="object-cover grayscale rounded-xl"
+                        />{" "}
+                    </div>
+                );
+            })}
+        </motion.div>
+    );
+};
